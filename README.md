4b624fe7a105bc640cdf9235ae54cf289958d3eb
Rob P <rob.p.tec@gmail.com>
2020-05-24 18:05:04 +0100

    initial work

---
+++ README.md
@@ -0,0 +1,80 @@
+# Property Price Register CLI (unoffical)
+
+This cli allows you to query the sales listed on `https://www.propertypriceregister.ie`
+
+## Installation
+
+Using go
+
+```
+$ go install github.com/robtec/ppr-gp/cmd/...
+```
+
+## Usage
+
+`ppr` supports the following options
+
+* `-c` - county to search, default Galway
+* `-yf` - year from to search, default 2010
+* `-yt` - year to search to, default 2020
+* `-a` - address to search
+* `-o` - change output, options are `json`, default `table`
+
+### Example
+
+```
+$ ppr -c mayo -yf 2019 -a "castlebar"
+
++------------+--------------------------------+----------------+
+|    DATE    |            ADDRESS             |     PRICE      |
++------------+--------------------------------+----------------+
+| 20/12/2019 | DUMMY, CASTLEBAR, MAYO         | €45,500.00     |
++------------+--------------------------------+----------------+
+| 19/12/2019 | LANE, CASTLEBAR, MAYO          | €321,000.00    |
++------------+--------------------------------+----------------+
+| 17/12/2019 | SPRINGFIELD, CASTLEBAR,        | €543,000.00    |
+|            | MAYO                           |                |
++------------+--------------------------------+----------------+
+| 17/12/2019 | MICKY MOUSE DRIVE, CASTLEBAR   | €99,000.00     |
++------------+--------------------------------+----------------+
+| 13/12/2019 | 123 FAKE ROAD, CASTLEBAR, CO   | €75,000.00     |
+|            | MAYO                           |                |
++------------+--------------------------------+----------------+
+# real addresses/prices omitted for privacy
+...
+```
+
+JSON output
+
+```
+$ ppr -o json -c mayo -yf 2019 -a "castlebar"
+
+{
+    "sales": [
+        {
+            "date": "20/12/2019",
+            "price": "€45,500.00",
+            "address": "DUMMY, CASTLEBAR, MAYO "
+        },
+        {
+            "date": "19/12/2019",
+            "price": "€321,000.00",
+            "address": "LANE, CASTLEBAR, MAYO"
+        },
+        {
+            "date": "17/12/2019",
+            "price": "€543,000.00",
+            "address": "SPRINGFIELD, CASTLEBAR, MAYO"
+        },
+        {
+            "date": "17/12/2019",
+            "price": "€99,000.00",
+            "address": "MICKY MOUSE DRIVE, CASTLEBAR"
+        },
+        {
+            "date": "13/12/2019",
+            "price": "€75,000.00",
+            "address": "123 FAKE ROAD, CASTLEBAR, CO MAYO"
+        }]
+}
+```
---
+++ cmd/ppr/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gocolly/colly"
+	"github.com/olekukonko/tablewriter"
+)
+
+type result struct {
+	Sales []sale `json:"sales"`
+	Total int    `json:"total"`
+	Query string `json:"query"`
+}
+
+type sale struct {
+	Date    string `json:"date"`
+	Price   string `json:"price"`
+	Address string `json:"address"`
+}
+
+func main() {
+
+	county := flag.String("c", "galway", "select county")
+	yearFrom := flag.String("yf", "2010", "select year to search from")
+	yearTo := flag.String("yt", "2020", "select year to search to")
+	address := flag.String("a", "2020", "select address to search")
+	output := flag.String("o", "table", "output to table (default) or json")
+
+	flag.Parse()
+
+	host := "www.propertypriceregister.ie"
+
+	fullHost := "https://" + host
+
+	path := "/Website/npsra/PPR/npsra-ppr.nsf/PPR-By-Date"
+
+	query := fmt.Sprintf("&Start=1&Query=[dt_execution_date]>=01/01/%s AND [dt_execution_date]<01/01/%s AND [address]=*%s* AND [dc_county]=%s", *yearFrom, *yearTo, *address, *county)
+
+	url := fullHost + path + query
+
+	var sales []sale
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(host),
+	)
+
+	c.WithTransport(&http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	})
+
+	c.OnHTML("#content", func(content *colly.HTMLElement) {
+
+		optionalNavURLs := content.ChildAttrs("#rcColRt > a", "href")
+
+		content.ForEach("table.resultsTable tr", func(_ int, el *colly.HTMLElement) {
+
+			if el.ChildText("td:nth-child(1)") != "" {
+
+				sale := sale{}
+
+				sale.Date = el.ChildText("td:nth-child(1)")
+				sale.Price = el.ChildText("td:nth-child(2)")
+				sale.Address = el.ChildText("td:nth-child(3)")
+
+				sales = append(sales, sale)
+			}
+		})
+
+		if len(optionalNavURLs) > 0 {
+			if len(optionalNavURLs) > 1 {
+				c.Visit(fullHost + optionalNavURLs[1])
+			} else {
+				c.Visit(fullHost + optionalNavURLs[0])
+			}
+		}
+	})
+
+	err := c.Post(url, map[string]string{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := result{sales, len(sales), query}
+
+	if *output == "json" {
+
+		var jsonData []byte
+		jsonData, err = json.MarshalIndent(result, "", "    ")
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(string(jsonData))
+	} else {
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Date", "Address", "Price"})
+		table.SetRowLine(true)
+
+		for _, s := range sales {
+			table.Append([]string{s.Date, s.Address, s.Price})
+		}
+
+		table.Render()
+	}
+}
