a7a58db0e254c95f9386862c982da972198b9b4e
robtec <rob.p.tec@gmail.com>
2020-05-31 14:26:01 +0100

    build

--- build.sh
+++ build.sh
@@ -2,22 +2,8 @@
 
 set -eux
 
-PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
-
-mkdir -p $PROJECT_ROOT
-rmdir $PROJECT_ROOT
-ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
-cd $PROJECT_ROOT
-go get -v ./...
-
 COMMIT_ID=$(git rev-parse --short HEAD)
 VERSION=$(cat VERSION)
-
-EXT=''
-
-if [ $GOOS == 'windows' ]; then
-EXT='.exe'
-fi
 
 go build -ldflags "-X main.Version=$(VERSION) -X main.CommitID=$(COMMIT_ID)" ./cmd/ppr
 
